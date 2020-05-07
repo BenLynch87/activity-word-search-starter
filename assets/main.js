1cc48fae128adbc3b4a2ee80de9a3df80a088740
Ben Lynch <blynchisonfire@gmail.com>
2020-05-07 16:31:00 -0400

    completed todos

--- assets/main.js
+++ assets/main.js
@@ -5,34 +5,32 @@
 // On our HTML page, we have a #search-button, a #textbox input,
 // box a #sentence, and a #search-results DIV.
 
-// TODO 1: Use querySelector to assign EACH of the above 
-// elements to a variable, just like this:
-let searchButton = document.querySelector('#search-button')
+let searchButton = document.querySelector('#search-button');
+let input = document.querySelector('#textbox');
+let results = document.querySelector('#search-results');
+let sentence = document.querySelector('#sentence').innerText.toLowerCase();
+let splitSentence = sentence.split(' ');
+let wordCount = 0;
 
 searchButton.addEventListener('click', function () {
- 
-  // TODO 2: Write an IF statement which determines whether
-  // the user-supplied string from #textbox is included in
-  // the #sentence string.
-  
-  // Hint 1: To get the user-supplied string from the input 
-  // box (#textbox), use the property .value on the variable
-  // you assigned the textbox element to.
-
-  // Hint 2: To get the string contained within #sentence, use
-  // the property .innerText on the variable you assigned the
-  // #sentence element to.
-
-  // Hint 3: You can check whether string1 contains string2
-  // by using string1.includes(string2) – which will return
-  // true or false. 
-
-  // TODO 3: If the user-supplied string is included in the
-  // #sentence string, update the innerText of #search-results
-  // with a success message (such as, "A match was found!"),
-  // otherwise update it with a failure message (such as, 
-  // "No results. Too bad!")
-
+  let inputFixed = input.value.trim().toLowerCase();
+  let inv = input.value;
+ if (sentence.includes(inputFixed)){
+    if (inputFixed.includes(' ')){
+      results.innerText = "A match was found!"
+    } else {
+      for (i = 0; i < splitSentence.length; i++){
+        if (splitSentence[i] === inputFixed){
+          wordCount++;
+        }
+      }
+      results.innerText = input.value + " found " + wordCount + " time(s)!";
+      wordCount = 0;
+    }
+    ;
+ } else {
+   results.innerText = "No results. Too bad!";
+ }
 })
 
 // STRETCH GOALS (easiest to hardest):
